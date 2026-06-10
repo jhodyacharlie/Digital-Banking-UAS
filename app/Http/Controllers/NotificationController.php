e59717780266eec3f0b678830b7d4492c54e6450
midnght228 <bungaapriyanti336@gmail.com>
2026-06-10 16:19:51 +0700

    //Bunga punya commit untuk fitur notification dan setting

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notification;
+use Illuminate\Http\Request;
+
+class NotificationController extends Controller
+{
+    public function index()
+    {
+        $notifications = Notification::all();
+
+        return view('notifications.index', compact('notifications'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PayController;
 use App\Http\Controllers\StatusController;
-
+use App\Http\Controllers\NotificationController;
 
 Route::get('/', function () {
     return redirect('/posts');
@@ -14,3 +14,4 @@ Route::resource('posts', PostController::class);
 Route::get('/payment', [PayController::class, 'index']);
 Route::post('/payment', [PayController::class, 'store']);
 Route::get('/status', [StatusController::class, 'index']);
+Route::get('/notifications', [NotificationController::class, 'index']);
